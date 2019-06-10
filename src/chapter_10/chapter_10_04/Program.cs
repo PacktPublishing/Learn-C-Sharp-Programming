c7b26fc2fc6857ffddccc3ea9cc33fc74fe6c465
Marius Bancila <marius.bancila@gmail.com>
2019-06-10 23:22:53 +0300

    linq examples

--- src/chapter_10/chapter_10_04/Program.cs
+++ src/chapter_10/chapter_10_04/Program.cs
@@ -29,6 +29,51 @@ namespace chapter_10_04
 
             }
          }
+
+         {
+            var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+            {
+               var count = text
+                  .Split(new char[] { ' ', ',', '.' })
+                  .Where(w => !string.IsNullOrEmpty(w))
+                  .Count();
+
+               var groups = text
+                  .Split(new char[] { ' ', ',', '.' })
+                  .GroupBy(w => w.Length, w => w.ToLower())
+                  .Select(g => new { Length = g.Key, Words = g })
+                  .Where(g => g.Length > 0)
+                  .OrderBy(g => g.Length);
+
+               foreach (var group in groups)
+               {
+                  Console.WriteLine($"Length={group.Length}");
+                  foreach (var word in group.Words)
+                  {
+                     Console.WriteLine($"  {word}");
+                  }
+               }
+            }
+
+            {
+               var count = (from w in text.Split(new char[] { ' ', ',', '.' })
+                            where !string.IsNullOrEmpty(w)
+                            select w).Count();
+
+               var groups = from w in text.Split(new char[] { ' ', ',', '.' })
+                            group w.ToLower() by w.Length into g
+                            where g.Key > 0
+                            orderby g.Key
+                            select new { Length = g.Key, Words = g };
+
+               foreach (var group in groups)
+               {
+                  Console.Write($"Length={group.Length}: ");
+                  Console.WriteLine(string.Join(',', group.Words));
+               }
+            }
+         }
       }
    }
 }
