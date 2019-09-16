9b91b0fa71161490738c6ff05b40a0d89a01e9ca
Marius Bancila <marius.bancila@gmail.com>
2019-09-17 01:04:55 +0300

    access control

---
+++ src/chapter_04/chapter_04/AccessModifiers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chapter_04
+{
+   public interface IEngine
+   {
+      double Power { get; }
+
+      void Start();
+   }
+
+   public class DieselEngine : IEngine
+   {
+      private double _power;
+      public double Power { get { return _power; } }
+
+      public void Start() { }
+   }
+}
