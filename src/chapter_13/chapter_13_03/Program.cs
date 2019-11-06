fb3a3a723d233a3fe4950d9463ea2e2ba253ee2e
Marius Bancila <marius.bancila@gmail.com>
2019-11-06 23:29:21 +0200

    xml read write

--- src/chapter_13/chapter_13_03/Program.cs
+++ src/chapter_13/chapter_13_03/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace chapter_13_03
@@ -53,12 +54,30 @@ namespace chapter_13_03
       {
          return (T)_serializer.Deserialize(stream);
       }
+
+      public static void Serialize(T value, XmlWriter stream, XmlSerializerNamespaces ns = null)
+      {
+         _serializer.Serialize(stream, value, ns);
+      }
+
+      public static T Deserialize(XmlReader stream)
+      {
+         return (T)_serializer.Deserialize(stream);
+      }
    }
 
    class Program
    {
       static void Main(string[] args)
       {
+         /*
+         <?xml version="1.0" encoding="utf-8"?>
+         <Employee xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+           <id>42</id>
+           <firstName>John</firstName>
+           <lastName>Doe</lastName>
+         </Employee>
+          */
          {
             var employee = new Employee
             {
@@ -67,7 +86,7 @@ namespace chapter_13_03
                LastName = "Doe"
             };
 
-            var path = Path.Combine(Path.GetTempPath(), "employee.xml");
+            var path = Path.Combine(Path.GetTempPath(), "employee1.xml");
             Serializer<Employee>.Serialize(employee, path);
             var result = Serializer<Employee>.Deserialize(path);
 
@@ -78,6 +97,18 @@ namespace chapter_13_03
             File.Delete(path);
          }
 
+         /*
+         <?xml version="1.0" encoding="utf-8"?>
+         <Employee xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+           <id>42</id>
+           <firstName>John</firstName>
+           <lastName>Doe</lastName>
+         </Employee>
+         <?xml version="1.0" encoding="utf-8"?>
+         <Department xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" Id="102">
+           <Name>IT</Name>
+         </Department>
+          */
          {
             var employee = new Employee
             {
@@ -92,14 +123,104 @@ namespace chapter_13_03
                Name = "IT"
             };
 
-            var path = Path.Combine(Path.GetTempPath(), "employee.xml");
+            var path = Path.Combine(Path.GetTempPath(), "employee2.xml");
             using (var wr = File.CreateText(path))
             {
                Serializer<Employee>.Serialize(employee, wr);
+               wr.WriteLine();
                Serializer<Department>.Serialize(department, wr);
             }
 
             Console.WriteLine(File.ReadAllText(path));
+
+            File.Delete(path);
+         }
+
+         /*
+         <?xml version="1.0" encoding="utf-8"?>
+         <Data>
+           <Employee>
+             <id>42</id>
+             <firstName>John</firstName>
+             <lastName>Doe</lastName>
+           </Employee>
+           <Department Id="102">
+             <Name>IT</Name>
+           </Department>
+         </Data>          
+          */
+         {
+            var employee = new Employee
+            {
+               EmployeeId = 42,
+               FirstName = "John",
+               LastName = "Doe"
+            };
+
+            var department = new Department
+            {
+               Id = 102,
+               Name = "IT"
+            };
+
+            var path = Path.Combine(Path.GetTempPath(), "employee3.xml");
+
+            var settings = new XmlWriterSettings 
+            { 
+               Encoding = Encoding.UTF8, 
+               Indent = true 
+            };
+
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using (var wr = XmlWriter.Create(path, settings))
+            {
+               wr.WriteStartDocument();
+               wr.WriteStartElement("Data");
+               Serializer<Employee>.Serialize(employee, wr, namespaces);
+               Serializer<Department>.Serialize(department, wr, namespaces);
+               wr.WriteEndElement();
+               wr.WriteEndDocument();
+            }
+
+            Console.WriteLine(File.ReadAllText(path));
+
+            var rdsettings = new XmlReaderSettings()
+            {
+               IgnoreComments = true,
+               IgnoreWhitespace = true
+            };
+
+            using (var rd = XmlReader.Create(path, rdsettings))
+            {
+               //rd.ReadStartElement("Data");
+             
+               while(rd.Read())
+               {
+                  switch(rd.NodeType)
+                  {
+                     case XmlNodeType.Element:
+                        Console.WriteLine();
+                        Console.Write($"<{rd.Name}");
+                        for(int i = 0; i < rd.AttributeCount; ++i)
+                        {
+                           var attribute = rd.GetAttribute(i);
+                        }
+                        Console.Write(">");
+                        break;
+                     case XmlNodeType.Text:
+                        Console.Write(rd.Value);
+                        break;
+                     case XmlNodeType.EndElement:
+                        Console.Write($"</{rd.Name}>");
+                        break;
+                     default:
+                        Console.WriteLine($"{rd.NodeType}: {rd.Value}");
+                        break;
+                  }
+               }
+            }
 
             File.Delete(path);
          }
