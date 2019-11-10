0305b4b03903fd9c7460fd6e5a5b417e4eaec592
Marius Bancila <marius.bancila@gmail.com>
2019-11-11 00:16:53 +0200

    json serialization

---
+++ src/chapter_13/chapter_13_04/Program.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace chapter_13_04
+{
+   public enum EmployeeStatus
+   {
+      Active,
+      Inactive
+   }
+
+   public class Employee
+   {
+      public int EmployeeId { get; set; }
+
+      public string FirstName { get; set; }
+
+      public string LastName { get; set; }
+
+      public DateTime? HireDate { get; set; }
+
+      public List<string> Telephones { get; set; }
+
+      public bool IsOnLeave { get; set; }
+      
+      [JsonConverter(typeof(StringEnumConverter))]
+      public EmployeeStatus Status { get; set; }
+
+      [JsonIgnore]
+      public DateTime LastModified { get; set; }
+
+      public override string ToString() => $"[{EmployeeId}] {LastName}, {FirstName}";
+   }
+   
+   class Program
+   {  
+      static void Main(string[] args)
+      {
+         var employee = new Employee
+         {
+            EmployeeId = 42,
+            FirstName = "John",
+            LastName = "Doe"
+         };
+
+         {
+            var text = JsonConvert.SerializeObject(employee);
+            Console.WriteLine(text);
+         }
+
+         {
+            var text = JsonConvert.SerializeObject(employee, Formatting.Indented);
+            Console.WriteLine(text);
+         }
+
+         {
+            var text = JsonConvert.SerializeObject(
+               employee,
+               Formatting.Indented,
+               new JsonSerializerSettings()
+               {
+                  NullValueHandling = NullValueHandling.Ignore,
+               });
+            Console.WriteLine(text);
+         }
+
+         {
+            var text = JsonConvert.SerializeObject(
+               employee,
+               Formatting.Indented,
+               new JsonSerializerSettings()
+               {
+                  NullValueHandling = NullValueHandling.Ignore,
+                  DefaultValueHandling = DefaultValueHandling.Ignore
+               });
+            Console.WriteLine(text);
+         }
+
+         {
+            var path = Path.Combine(Path.GetTempPath() + "employee.json");
+            var serializer = new JsonSerializer()
+            {
+               Formatting = Formatting.Indented,
+               NullValueHandling = NullValueHandling.Ignore,
+               DefaultValueHandling = DefaultValueHandling.Ignore
+            };
+
+            using (var sw = File.CreateText(path))
+            using (var jw = new JsonTextWriter(sw))
+            {
+               serializer.Serialize(jw, employee);
+            }
+
+            Console.WriteLine(File.ReadAllText(path));
+
+            using (var sr = File.OpenText(path))
+            using (var jr = new JsonTextReader(sr))
+            {
+               var result = serializer.Deserialize<Employee>(jr);
+
+               Console.WriteLine(result);
+            }
+
+            File.Delete(path);
+         }
+
+         var json =
+@"{
+  ""EmployeeId"": 42,
+  ""FirstName"": ""John"",
+  ""LastName"": ""Doe""
+}";
+         Console.WriteLine(json);
+
+         {
+            var result = JsonConvert.DeserializeObject<Employee>(json);
+
+            Console.WriteLine(result);
+         }
+      }
+   }
+}
---
+++ src/chapter_13/chapter_13_05/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace chapter_13_05
+{
+   public enum EmployeeStatus
+   {
+      Active,
+      Inactive
+   }
+
+   public class Employee
+   {
+      public int EmployeeId { get; set; }
+
+      public string FirstName { get; set; }
+
+      public string LastName { get; set; }
+
+      public DateTime? HireDate { get; set; }
+
+      public List<string> Telephones { get; set; }
+
+      public bool IsOnLeave { get; set; }
+
+      [JsonConverter(typeof(JsonStringEnumConverter))]
+      public EmployeeStatus Status { get; set; }
+
+      [JsonIgnore]
+      public DateTime LastModified { get; set; }
+
+      public override string ToString() => $"[{EmployeeId}] {LastName}, {FirstName}";
+   }
+
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var employee = new Employee
+         {
+            EmployeeId = 42,
+            FirstName = "John",
+            LastName = "Doe"
+         };
+
+         {
+            var text = JsonSerializer.Serialize(employee);
+            Console.WriteLine(text);
+         }
+
+         {
+            var text = JsonSerializer.Serialize(
+               employee,
+               new JsonSerializerOptions()
+               {
+                  WriteIndented = true,
+               });
+            Console.WriteLine(text);
+         }
+
+         {
+            var text = JsonSerializer.Serialize(
+               employee,
+               new JsonSerializerOptions()
+               {
+                  WriteIndented = true,
+                  IgnoreNullValues = true                  
+               });
+            Console.WriteLine(text);
+         }
+
+         {
+            var path = Path.Combine(Path.GetTempPath() + "employee.json");
+            var options = new JsonWriterOptions()
+            {
+               Indented = true,
+
+            };
+
+            using (var sw = File.CreateText(path))
+            using (var jw = new Utf8JsonWriter(sw.BaseStream, options))
+            {
+               jw.WriteStartObject();
+               jw.WriteNumber("EmployeeId", 42);
+               jw.WriteString("FirstName", "John");
+               jw.WriteString("LastName", "Doe");
+               jw.WriteBoolean("IsOnLeave", false);
+               jw.WriteString("Status", EmployeeStatus.Active.ToString());
+               jw.WriteEndObject();
+            }
+
+            var text = File.ReadAllText(path);
+            Console.WriteLine(text);
+
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            Utf8JsonReader reader = new Utf8JsonReader(data, isFinalBlock: true, state: default);
+
+            while (reader.Read())
+            {
+               switch (reader.TokenType)
+               {
+                  case JsonTokenType.PropertyName:
+                     Console.Write($@"""{reader.GetString()}"" : ");
+                     break;
+                  case JsonTokenType.String:
+                     {
+                        Console.WriteLine($"{reader.GetString()},");
+                        break;
+                     }
+
+                  case JsonTokenType.Number:
+                     {
+                        Console.WriteLine($"{reader.GetInt32()},");
+                        break;
+                     }
+                  case JsonTokenType.False:
+                  case JsonTokenType.True:
+                     {
+                        Console.WriteLine($"{reader.GetBoolean()},");
+                        break;
+                     }
+               }
+            }
+
+            File.Delete(path);
+         }
+      }
+   }
+}
