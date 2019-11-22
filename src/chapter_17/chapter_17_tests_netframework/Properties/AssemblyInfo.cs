26faa8a5699a783d48ee16adbac693da5e7c6cf6
Marius Bancila <marius.bancila@gmail.com>
2019-11-22 23:00:03 +0200

    data drivver tests in .NET framework

---
+++ src/chapter_17/chapter_17_tests_netframework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("chapter_17_tests_netframework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("chapter_17_tests_netframework")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9197b5b-36cb-402c-9184-ad01f3547384")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/chapter_17/chapter_17_tests_netframework/RectangleDataDrivenTests.cs
+++ src/chapter_17/chapter_17_tests_netframework/RectangleDataDrivenTests.cs
@@ -1,12 +1,21 @@
-﻿using chapter_17_lib;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
 using System.Collections.Generic;
+using chapter_17_lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace chapter_17_tests
+namespace chapter_17_tests_netframework
 {
    [TestClass]
    public class RectangleDataDrivenTests
    {
+      private static TestContext _tc;
+
+      [ClassInitialize]
+      public static void Initialization(TestContext testContext)
+      {
+         _tc = testContext;
+      }
+
       [DataTestMethod]
       [DataRow(true, 3, 4, 5, 6)]
       [DataRow(true, 5, 10, 20, 13)]
@@ -38,6 +47,27 @@ namespace chapter_17_tests
             result,
             rectangle.IntersectsWith(
                new Rectangle(left, top, right, bottom)));
-      }     
+      }
+
+      [DataTestMethod]
+      [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+                  "TestData.csv",
+                  "TestData#csv",
+                  DataAccessMethod.Sequential)]
+      public void TestIntersectsWith_CsvData()
+      {
+         var rectangle = new Rectangle(1, 2, 10, 12);
+
+         bool result = Convert.ToBoolean(_tc.DataRow["Expected"]);
+         int left = Convert.ToInt32(_tc.DataRow["left"]);
+         int top = Convert.ToInt32(_tc.DataRow["top"]);
+         int right = Convert.ToInt32(_tc.DataRow["right"]);
+         int bottom = Convert.ToInt32(_tc.DataRow["bottom"]);
+
+         Assert.AreEqual(
+            result,
+            rectangle.IntersectsWith(
+               new Rectangle(left, top, right, bottom)));
+      }
    }
 }
